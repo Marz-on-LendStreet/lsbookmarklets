6b65b27302beef7c4b612c4b1a0cb2fd0ae47c07
Marzban Doctor <marzban.doctor@lendstreet.com>
2018-08-07 15:36:14 -0700

    and more experimentation.

--- lscontactinfo.js
+++ lscontactinfo.js
@@ -12,16 +12,16 @@ else {
 }
 function fillForm() {
     //alert('In fillForm()');
-    $('input[placeholder="First name"]').val('Joe').trigger('input');
+//    $('input[placeholder="First name"]').val('Joe').trigger('input');
     $('input[placeholder="Last name"]').val('Earnest').trigger('input');
     $('input[placeholder="Email"]').val('joe.earnest.001@lendstreet.com').trigger('input');
     $('input[placeholder="Cell phone"]').val('5108675309').trigger('input');
     $('input[placeholder="Street address"]').val('1101 CORRAL STREET').trigger('input');
     $('input[placeholder="City"]').val('SWEETWATER').trigger('input');
     $('input[placeholder="Zip code"]').val('795566611').trigger('input');
-    $('input[ng-model]').trigger('input');
-    var sc=angular.element("[ng-controller=SiteController]").scope();
-    var m=sc.borrower;
+//    $('input[ng-model]').trigger('input');
+    var sc = angular.element("[ng-controller=SiteController]").scope();
+    var m = sc.borrower.contact;
     m.contact.first_name = 'Joe';
     sc.$apply();
     

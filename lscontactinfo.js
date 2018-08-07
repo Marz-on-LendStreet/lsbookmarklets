dcafb300b40e6ce67514c5781ccd42e19f223f4e
Marzban Doctor <marzban.doctor@lendstreet.com>
2018-08-07 14:43:33 -0700

    more experimentation.

--- lscontactinfo.js
+++ lscontactinfo.js
@@ -13,13 +13,13 @@ else {
 function fillForm() {
     //alert('In fillForm()');
     $('input[placeholder="First name"]').val('Joe');
-    $('input[placeholder="First name"]').trigger('input');
     $('input[placeholder="Last name"]').val('Earnest');
     $('input[placeholder="Email"]').val('joe.earnest.001@lendstreet.com');
     $('input[placeholder="Cell phone"]').val('5108675309');
     $('input[placeholder="Street address"]').val('1101 CORRAL STREET');
     $('input[placeholder="City"]').val('SWEETWATER');
     $('input[placeholder="Zip code"]').val('795566611');
+    $('input[ng-model]').trigger('input');
     //var state = 'option[text="Texas"]';
     //$('select[data-test="state"]').find(state).attr("selected", "selected");
 }

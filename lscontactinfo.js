7cbad3a9607e0785f54bcf964fc832750bb49128
Marzban Doctor <marzban.doctor@lendstreet.com>
2018-08-07 14:37:28 -0700

    more changes/experimentation.

--- lscontactinfo.js
+++ lscontactinfo.js
@@ -11,14 +11,15 @@ else {
     fillForm();
 }
 function fillForm() {
-    alert('In fillForm()');
+    //alert('In fillForm()');
     $('input[placeholder="First name"]').val('Joe');
+    $('input[placeholder="First name"]').trigger('input');
     $('input[placeholder="Last name"]').val('Earnest');
-    $('input[placeholder="Email"]').val('je001@mdls.com');
+    $('input[placeholder="Email"]').val('joe.earnest.001@lendstreet.com');
     $('input[placeholder="Cell phone"]').val('5108675309');
     $('input[placeholder="Street address"]').val('1101 CORRAL STREET');
     $('input[placeholder="City"]').val('SWEETWATER');
     $('input[placeholder="Zip code"]').val('795566611');
-    var state = 'option[text="Texas"]';
-    $('select[data-test="state"]').find(state).attr("selected", "selected");
+    //var state = 'option[text="Texas"]';
+    //$('select[data-test="state"]').find(state).attr("selected", "selected");
 }
